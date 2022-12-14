9d7bc14b6042473bb13ae0ec3c53556602e08048
Fangtangtang <2167295472@qq.com>
2022-12-15 06:49:47 +0800

    /Makefile

---
+++ cmake-build-debug/CMakeFiles/qianzhi.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qianzhi.dir/element.cpp.o"
+  "CMakeFiles/qianzhi.dir/element.cpp.o.d"
+  "CMakeFiles/qianzhi.dir/main.cpp.o"
+  "CMakeFiles/qianzhi.dir/main.cpp.o.d"
+  "qianzhi"
+  "qianzhi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qianzhi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
